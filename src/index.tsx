582eedd8b0e667c3cd8eef1694fcb3a8a8049096
Josh Wren <josh.wren@life.church>
2020-12-14 09:31:58 -0600

    Added title component
    * fixed bug with theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Content, Title } from './components';
 import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
   <>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div>hello world</div>
+      <Content data-cy="content">
+        <Title datay-cy="title">Sudoku</Title>
+        <div>hello world</div>
+      </Content>
     </ThemeProvider>
   </>,
   document.getElementById('root')
