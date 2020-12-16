16114aec7494508f8cf7859438473fc10bb9f845
Josh Wren <josh.wren@life.church>
2020-12-16 09:30:16 -0600

    added some of the logic for sudoku
    * also added test for shuffler

---
+++ src/utils/fill-grid/index.ts
@@ -0,0 +1,43 @@
+import { GRID, NUMBERS } from 'typings';
+import shuffle from '../shuffle';
+
+const gridExample: GRID = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function fillGrid(grid: GRID) {
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers);
+      for (let value of numbers) {
+        // is it not in grid row?
+        // is it not in grid col?
+        // is it not in the grid square
+        // if so
+        grid[row][col] = value;
+        // check grid if its is full if yes stop and return
+        // otherwise we run fullgrid()
+      }
+      break;
+    }
+  }
+  grid[row][col] = 0;
+}
+
+export default fillGrid;
